2f0b563589f8009f956238c9c6c4b7e86e217c77
Joel Brewster <j.n.brewster@gmail.com>
2016-08-15 19:40:18 +1000

    formatting

--- javascript.js
+++ javascript.js
@@ -1,43 +1,41 @@
-$(document).ready(function(){
-    animateDiv();
+$(document).ready(function() {
 
+  animateDiv();
 });
 
-function makeNewPosition(){
+function makeNewPosition() {
 
-    // Get viewport dimensions (remove the dimension of the div)
-    var h = $(window).height() - 1;
-    var w = $(window).width() - 2;
+  // Get viewport dimensions (remove the dimension of the div)
+  var h = $(window).height() - 1;
+  var w = $(window).width() - 2;
 
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
+  var nh = Math.floor(Math.random() * h);
+  var nw = Math.floor(Math.random() * w);
 
-    return [nh,nw];
-
+  return [nh,nw];
 }
 
-function animateDiv(){
-    var newq = makeNewPosition();
-    var oldq = $('.bug').offset();
-    var speed = calcSpeed([oldq.top, oldq.left], newq);
+function animateDiv() {
 
-    $('.bug').animate({ top: newq[0], left: newq[1] }, speed, function(){
-      animateDiv();
-    });
+  var newq = makeNewPosition();
+  var oldq = $('.bug').offset();
+  var speed = calcSpeed([oldq.top, oldq.left], newq);
 
+  $('.bug').animate({ top: newq[0], left: newq[1] }, speed, function(){
+    animateDiv();
+  });
 }
 
 function calcSpeed(prev, next) {
+  var x = Math.abs(prev[1] - next[1]);
+  var y = Math.abs(prev[0] - next[0]);
 
-    var x = Math.abs(prev[1] - next[1]);
-    var y = Math.abs(prev[0] - next[0]);
+  var greatest = x > y ? x : y;
 
-    var greatest = x > y ? x : y;
+  var speedModifier = 0.07;
 
-    var speedModifier = 0.07;
+  var speed = Math.ceil(greatest/speedModifier);
 
-    var speed = Math.ceil(greatest/speedModifier);
-
-    return speed;
-
+  return speed;
 }
+
