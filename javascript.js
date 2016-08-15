7bea1683c3bbf6a31728a024c7c2697f0651bd80
Joel Brewster <j.n.brewster@gmail.com>
2016-08-16 00:20:55 +1000

    Changed bug speed

--- javascript.js
+++ javascript.js
@@ -32,7 +32,7 @@ function calcSpeed(prev, next) {
 
   var greatest = x > y ? x : y;
 
-  var speedModifier = 0.07;
+  var speedModifier = 0.02;
 
   var speed = Math.ceil(greatest/speedModifier);
 
