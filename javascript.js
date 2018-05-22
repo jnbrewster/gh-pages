b148f712f0524c2ded52d1b3a2a2f073dd3fb69f
Joel Brewster <joel@BirthdayBook-Pro.local>
2018-05-22 22:17:15 +1000

    Welcome to 2018

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
-window.onload = function() {
+window.onload = function () {
+    $('body').flowtype({
+        minFont : 12,
+        maxFont : 18
+    });
+
     document.body.className += " loaded";
     $(".profile-image-solid").css("opacity", "1");
     $(".profile-image-lines").css("opacity", ".1");
+
     new Vivus('my-svg', {
-        duration: 6000
+        duration: 4000
     });
 }
