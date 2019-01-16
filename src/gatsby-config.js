4ef9194b999b29f71b85cf18078bea384afd7a94
Joel Brewster <hi@joelbrewster.com>
2019-01-17 09:11:50 +1100

    Move site to gatsby

---
+++ src/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Joel Brewster',
+    titleTemplate: '',
+    description: 'Landing page for Joel Brewster',
+    url: 'http.joelbrewster.com',
+    image: '/favicon.ico',
+    pathPrefix: '/'
+  }
+};
